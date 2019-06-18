588a4284c522ab47597783e49bb8ba076be2e8c2
Brian Gerstle <brian.gerstle@gmail.com>
2019-06-17 21:13:21 -0500

    Interesting test failure on merge w/ large differences in elements

--- ruby/lib/merge_sort.rb
+++ ruby/lib/merge_sort.rb
@@ -1,12 +1,47 @@
 module MergeSort
   def self.sort(xs)
-    xs
+    return xs if xs.count == 1
+
+    first_half, second_half = split(xs)
+
+    sorted_first_half = sort(first_half)
+    sorted_second_half = sort(second_half)
+
+    merge(sorted_first_half, sorted_second_half)
   end
 
   def self.split(xs)
-    return xs if xs.count == 1
-    middle = floor(xs.count / 2)
-    [xs[0..middle], xs[middle..-1]]
+    return xs if xs.count <= 1
+    middle = (xs.count / 2).floor
+    [xs[0...middle], xs[middle..-1]]
+  end
+
+  def self.merge(xs, ys)
+    xs_enum = xs.to_enum
+    ys_enum = ys.to_enum
+    result = []
+    while xs_enum.has_next? || ys_enum.has_next?
+      if !ys_enum.has_next?
+        result << xs_enum.next
+      elsif !xs_enum.has_next?
+        result << ys_enum.next
+      elsif xs_enum.peek <= ys_enum.peek
+        result << xs_enum.next
+      else
+        result << ys_enum.next
+      end
+    end
+    result
   end
 end
 
+class Enumerator
+  def has_next?
+    begin
+      _ = peek
+      return true
+    rescue StopIteration
+      return false
+    end
+  end
+end
--- ruby/spec/merge_sort_spec.rb
+++ ruby/spec/merge_sort_spec.rb
@@ -12,5 +12,43 @@ describe MergeSort do
     it 'returns the same array if it has 1 element' do
       expect(MergeSort.split([0])).to eq([0])
     end
+
+    it 'returns arrays of the first and second element of a 2-element array' do
+      expect(MergeSort.split([0, 1])).to eq([[0], [1]])
+    end
+
+    it 'returns a larger second array for inputs w/ odd counts' do
+      expect(MergeSort.split([0, 1, 2])).to eq([[0], [1, 2]])
+    end
+
+    it 'splits arrays with even counts down the middle' do
+      expect(MergeSort.split([0, 1, 2, 3])).to eq([[0, 1], [2, 3]])
+    end
+
+    it 'returns an empty array if input is empty' do
+      expect(MergeSort.split([])).to eq([])
+    end
+  end
+
+  describe 'merge' do
+    it 'combines two 1-element array elements in ascending order' do
+      expect(MergeSort.merge([0], [1])).to eq([0, 1])
+    end
+
+    it 'combines two 1-element array elements in ascending order when given out of order' do
+      expect(MergeSort.merge([1], [0])).to eq([0, 1])
+    end
+
+    it 'combines array elements in ascending order when lesser element alternates between arrays' do
+      expect(MergeSort.merge([1, 3], [0, 2])). to eq([0, 1, 2, 3])
+    end
+
+    it 'combines array elements in ascending order when lesser element is in the same array' do
+      expect(MergeSort.merge([0, 1], [2, 3])).to eq([0, 1, 2, 3])
+    end
+
+    it 'combines array elements in ascending order when there are large differences in the elements' do
+      expect(MergeSort.merge([1, 100], [50, 5])).to eq([1, 5, 50, 100])
+    end
   end
 end

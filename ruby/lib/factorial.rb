0a1277c339b59ead60ebdfc052a4d438390bfaa8
Brian Gerstle <brian.gerstle@gmail.com>
2019-06-19 07:38:47 -0500

    Finished factorial
    Using iterative "while" implementation

--- ruby/lib/factorial.rb
+++ ruby/lib/factorial.rb
@@ -1,6 +1,13 @@
 module Factorial
   def factorial
-    self
+    return self if self <= 1
+    decrement = 1
+    result = self
+    while self - decrement > 1
+      result *= self - decrement
+      decrement += 1
+    end
+    result
   end
 end
 
--- ruby/spec/factorial_spec.rb
+++ ruby/spec/factorial_spec.rb
@@ -20,5 +20,17 @@ describe Factorial do
     it 'returns 2 for 2' do
       expect(2.factorial).to eq(2)
     end
+
+    it 'returns 6 for 3' do
+      expect(3.factorial).to eq(6)
+    end
+
+    it 'returns 3628800 for 10' do
+      expect(10.factorial).to eq(3628800)
+    end
+
+    it 'handles big numbers' do
+      expect(100.factorial).to eq(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
+    end
   end
 end

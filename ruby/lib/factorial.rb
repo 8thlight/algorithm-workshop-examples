240471eb463cfc3d71c9a45f3779e4c0a9f9b302
Brian Gerstle <brian.gerstle@gmail.com>
2019-06-19 07:41:40 -0500

    Refactor factorial to be recursive

--- ruby/lib/factorial.rb
+++ ruby/lib/factorial.rb
@@ -1,13 +1,7 @@
 module Factorial
   def factorial
     return self if self <= 1
-    decrement = 1
-    result = self
-    while self - decrement > 1
-      result *= self - decrement
-      decrement += 1
-    end
-    result
+    self * (self - 1).factorial
   end
 end
 

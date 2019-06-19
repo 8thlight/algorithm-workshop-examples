6568634c8ac03e848ee0b4f5a3df7f388789faea
Brian Gerstle <brian.gerstle@gmail.com>
2019-06-19 07:43:37 -0500

    Add test case for negative numbers

--- ruby/spec/factorial_spec.rb
+++ ruby/spec/factorial_spec.rb
@@ -32,5 +32,9 @@ describe Factorial do
     it 'handles big numbers' do
       expect(100.factorial).to eq(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
     end
+
+    it 'does nothing for negative numbers' do
+      expect(-1.factorial).to eq(-1)
+    end
   end
 end

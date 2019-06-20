119b5b77fd6442a879dbb8762a5f7b4a7f89a3ba
Brian Gerstle <brian.gerstle@gmail.com>
2019-06-20 00:19:23 -0500

    [ruby:merge-sort] Add iterative and recursive implementations

--- ruby/lib/factorial.rb
+++ ruby/lib/factorial.rb
@@ -3,6 +3,18 @@ module Factorial
     return self if self <= 1
     self * (self - 1).factorial
   end
+
+  def iterative_factorial
+    return self if self <= 1
+    decrement = 1
+    result = self
+    while self - decrement > 1
+      result *= self - decrement
+      decrement += 1
+    end
+    result
+
+  end
 end
 
 class Integer
--- ruby/spec/factorial_spec.rb
+++ ruby/spec/factorial_spec.rb
@@ -8,33 +8,41 @@ describe Factorial do
     end
   end
 
-  describe 'factorial' do
+  shared_examples_for 'factorial' do |factorial_method:|
     it 'returns 0 for 0' do
-      expect(0.factorial).to eq(0)
+      expect(0.send(factorial_method)).to eq(0)
     end
 
     it 'returns 1 for 1' do
-      expect(1.factorial).to eq(1)
+      expect(1.send(factorial_method)).to eq(1)
     end
 
     it 'returns 2 for 2' do
-      expect(2.factorial).to eq(2)
+      expect(2.send(factorial_method)).to eq(2)
     end
 
     it 'returns 6 for 3' do
-      expect(3.factorial).to eq(6)
+      expect(3.send(factorial_method)).to eq(6)
     end
 
     it 'returns 3628800 for 10' do
-      expect(10.factorial).to eq(3628800)
+      expect(10.send(factorial_method)).to eq(3628800)
     end
 
     it 'handles big numbers' do
-      expect(100.factorial).to eq(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
+      expect(100.send(factorial_method)).to eq(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
     end
 
     it 'does nothing for negative numbers' do
-      expect(-1.factorial).to eq(-1)
+      expect(-1.send(factorial_method)).to eq(-1)
     end
+  end
+
+  describe 'recursive_factorial' do
+    it_behaves_like 'factorial', { factorial_method: :factorial }
+  end
+
+  describe 'iterative_factorial' do
+    it_behaves_like 'factorial', { factorial_method: :iterative_factorial }
   end
 end

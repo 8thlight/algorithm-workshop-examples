73485ce5b9a8bb2508587bcb0edd015322ad179d
Brian Gerstle <brian.gerstle@gmail.com>
2019-11-10 20:02:23 -0600

    python: merge sort

---
+++ python/algorithms/merge_sort.py
@@ -0,0 +1,43 @@
+from typing import Tuple, List, TypeVar, Any
+
+from math import floor
+
+E = TypeVar('E')
+
+def merge_sort(xs: List[E]):
+    if len(xs) < 2:
+        return xs
+    left, right = split(xs)
+    s_left = merge_sort(left)
+    s_right = merge_sort(right)
+    return merge_sorted(s_left, s_right)
+
+
+def split(xs: List[E]) -> Tuple[List[E], List[E]]:
+    middle = floor(len(xs) / 2)
+    return (xs[:middle], xs[middle:])
+
+
+def merge_sorted(xs: List[Any], ys: List[Any]) -> List[Any]:
+    merged = []
+    ixs = 0
+    len_xs = len(xs)
+    iys = 0
+    len_ys = len(ys)
+    while ixs < len_xs or iys < len_ys:
+        x = xs[ixs] if ixs < len_xs else None
+        y = ys[iys] if iys < len_ys else None
+        if x is not None and y is not None:
+            if x < y:
+                merged.append(x)
+                ixs += 1
+            else:
+                merged.append(y)
+                iys += 1
+        elif x is None:
+            merged.append(y)
+            iys += 1
+        else:
+            merged.append(x)
+            ixs += 1
+    return merged
---
+++ python/tests/test_merge_sort.py
@@ -0,0 +1,75 @@
+from algorithms.merge_sort import merge_sort, split, merge_sorted
+
+import pytest
+from typing import List, Tuple
+
+def test_empty():
+    assert merge_sort([]) == []
+
+
+def test_singleton():
+    assert merge_sort([0]) == [0]
+
+
+def test_ordered():
+    assert merge_sort([0, 1]) == [0, 1]
+
+
+def test_reversed():
+    assert merge_sort([1, 0]) == [0, 1]
+
+
+def test_unordered():
+    assert merge_sort([1, 0, 2]) == [0, 1, 2]
+
+
+def test_split_empty():
+    assert split([]) == ([], [])
+
+
+def test_split_singleton():
+    assert split([0]) == ([], [0])
+
+
+def test_split_even():
+    assert split([0, 1]) == ([0], [1])
+
+
+def test_split_odd():
+    assert split([0, 1, 2]) == ([0], [1, 2])
+
+
+def test_merging_empty():
+    assert merge_sorted([], []) == []
+
+
+def test_merging_second_empty():
+    assert merge_sorted([0], []) == [0]
+
+
+def test_merging_first_empty():
+    assert merge_sorted([], [0]) == [0]
+
+
+def test_merging_one_each():
+    assert merge_sorted([0], [1]) == [0, 1]
+
+
+def test_merging_one_each_reversed():
+    assert merge_sorted([1], [0]) == [0, 1]
+
+
+def test_merging_left_left_right():
+    assert merge_sorted([0, 1], [2]) == [0, 1, 2]
+
+
+def test_merging_left_right_left():
+    assert merge_sorted([0, 2], [1]) == [0, 1, 2]
+
+
+def test_merging_right_left_right():
+    assert merge_sorted([1], [0, 2]) == [0, 1, 2]
+
+
+def test_merging_right_right_left():
+    assert merge_sorted([2], [0, 1]) == [0, 1, 2]

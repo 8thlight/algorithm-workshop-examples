f8c3f2e1c1bf62e6d63687d9ced881a77e6dc677
Brian Gerstle <brian.gerstle@gmail.com>
2019-11-10 19:19:06 -0600

    Python factorial

--- python/algorithms/factorial.py
+++ python/algorithms/factorial.py
@@ -1,2 +1,14 @@
-def factorial(x):
-    return x
+from functools import reduce
+
+def factorial(x: int) -> int:
+    if x < 3:
+        return x
+    return x * factorial(x - 1)
+
+
+def iter_factorial(x: int) -> int:
+    if x < 3:
+        return x
+    xs = range(x - 1, 1, -1)
+    return reduce(lambda x1, x2: x1 * x2, xs, x)
+    
--- python/tests/test_factorial.py
+++ python/tests/test_factorial.py
@@ -1,4 +1,22 @@
-from algorithms.factorial import factorial
+from algorithms.factorial import factorial, iter_factorial
+import pytest
+from typing import Tuple, List
 
-def test_zero():
-    assert factorial(0) == 0
+EXPECTED_OUTPUTS: List[Tuple[int, int]] = [
+        (0, 0), 
+        (1, 1), 
+        (2, 2), 
+        (3, 6),
+        (10, 3628800),
+        (100, 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
+    ]
+
+@pytest.mark.parametrize("expected_outputs", EXPECTED_OUTPUTS)
+def test_examples(expected_outputs):
+    x, expected = expected_outputs
+    assert factorial(x) == expected
+
+@pytest.mark.parametrize("expected_outputs", EXPECTED_OUTPUTS)
+def test_examples_iterative(expected_outputs):
+    x, expected = expected_outputs
+    assert iter_factorial(x) == expected

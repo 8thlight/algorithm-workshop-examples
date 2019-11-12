5243c4fe558ce47edabdcb4e28f3454fbd32192f
Brian Gerstle <brian.gerstle@gmail.com>
2019-11-11 21:40:14 -0600

    Tweak python factorial

--- python/algorithms/factorial.py
+++ python/algorithms/factorial.py
@@ -1,14 +1,13 @@
 from functools import reduce
+from operator import mul
 
 def factorial(x: int) -> int:
     if x < 3:
         return x
     return x * factorial(x - 1)
 
-
 def iter_factorial(x: int) -> int:
     if x < 3:
         return x
-    xs = range(x - 1, 1, -1)
-    return reduce(lambda x1, x2: x1 * x2, xs, x)
-    
+    xs = range(2, x)
+    return reduce(mul, xs, x)

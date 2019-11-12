eec265ebf6ff310b06459b97b0304bd9dc1a1682
Brian Gerstle <brian.gerstle@gmail.com>
2019-11-12 12:51:05 -0600

    Parametrize tests for iterative & recusrive merge_sorted

--- python/tests/test_merge_sort.py
+++ python/tests/test_merge_sort.py
@@ -1,4 +1,4 @@
-from algorithms.merge_sort import merge_sort, split, merge_sorted
+from algorithms.merge_sort import merge_sort, split, merge_sorted, rec_merge_sorted
 
 import pytest
 from typing import List, Tuple
@@ -33,29 +33,40 @@ def test_split_even():
 def test_split_odd():
     assert split([0, 1, 2]) == ([0], [1, 2])
 
-def test_merging_empty():
-    assert merge_sorted([], []) == []
+param_merge_sorted_impls = pytest.mark.parametrize("f", [merge_sorted, rec_merge_sorted])
 
-def test_merging_second_empty():
-    assert merge_sorted([0], []) == [0]
+@param_merge_sorted_impls
+def test_merging_empty(f):
+    assert f([], []) == []
 
-def test_merging_first_empty():
-    assert merge_sorted([], [0]) == [0]
+@param_merge_sorted_impls
+def test_merging_second_empty(f):
+    assert f([0], []) == [0]
 
-def test_merging_one_each():
-    assert merge_sorted([0], [1]) == [0, 1]
+@param_merge_sorted_impls
+def test_merging_first_empty(f):
+    assert f([], [0]) == [0]
 
-def test_merging_one_each_reversed():
-    assert merge_sorted([1], [0]) == [0, 1]
+@param_merge_sorted_impls
+def test_merging_one_each(f):
+    assert f([0], [1]) == [0, 1]
 
-def test_merging_left_left_right():
-    assert merge_sorted([0, 1], [2]) == [0, 1, 2]
+@param_merge_sorted_impls
+def test_merging_one_each_reversed(f):
+    assert f([1], [0]) == [0, 1]
 
-def test_merging_left_right_left():
-    assert merge_sorted([0, 2], [1]) == [0, 1, 2]
+@param_merge_sorted_impls
+def test_merging_left_left_right(f):
+    assert f([0, 1], [2]) == [0, 1, 2]
 
-def test_merging_right_left_right():
-    assert merge_sorted([1], [0, 2]) == [0, 1, 2]
+@param_merge_sorted_impls
+def test_merging_left_right_left(f):
+    assert f([0, 2], [1]) == [0, 1, 2]
 
-def test_merging_right_right_left():
-    assert merge_sorted([2], [0, 1]) == [0, 1, 2]
+@param_merge_sorted_impls
+def test_merging_right_left_right(f):
+    assert f([1], [0, 2]) == [0, 1, 2]
+
+@param_merge_sorted_impls
+def test_merging_right_right_left(f):
+    assert f([2], [0, 1]) == [0, 1, 2]

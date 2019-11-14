658e738d1b71afe594fbce09b01ccb8a8cba9252
Brian Gerstle <brian.gerstle@gmail.com>
2019-11-13 19:24:52 -0600

    refactor factorial tests

--- python/tests/test_factorial.py
+++ python/tests/test_factorial.py
@@ -1,22 +1,19 @@
-from algorithms.factorial import factorial, iter_factorial
 import pytest
-from typing import Tuple, List
 
-EXPECTED_OUTPUTS: List[Tuple[int, int]] = [
-        (0, 0), 
-        (1, 1), 
-        (2, 2), 
-        (3, 6),
-        (10, 3628800),
-        (100, 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
-    ]
+from algorithms.factorial import factorial
 
-@pytest.mark.parametrize("expected_outputs", EXPECTED_OUTPUTS)
-def test_examples(expected_outputs):
-    x, expected = expected_outputs
-    assert factorial(x) == expected
+@pytest.mark.parametrize("x", [0, 1, 2])
+def test_identical_factorials(x):
+    assert factorial(x) == x
 
-@pytest.mark.parametrize("expected_outputs", EXPECTED_OUTPUTS)
-def test_examples_iterative(expected_outputs):
-    x, expected = expected_outputs
-    assert iter_factorial(x) == expected
+def test_three():
+    assert factorial(3) == 3 * 2
+
+def test_four():
+    assert factorial(4) == 4 * 3 * 2
+
+def test_five():
+    assert factorial(5) == 5 * 4 * 3 * 2
+
+def test_one_hundred():
+    assert factorial(100) == 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
